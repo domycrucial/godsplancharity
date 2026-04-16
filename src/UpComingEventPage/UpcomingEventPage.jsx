09cf82b702a042f1e05f53f82e214756eb57ef57
domycrucial <chuwadominic52@gmail.com>
2026-04-16 14:51:53 +0300

    improved meta features for seo

--- src/UpComingEventPage/UpcomingEventPage.jsx
+++ src/UpComingEventPage/UpcomingEventPage.jsx
@@ -15,11 +15,16 @@ export default function UpcomingEvent(){
     return(
         <>  
                 <Helmet>
+                <h1>Charity Outreach Event in Namanga 2026</h1>
                 <title>Upcoming Event | Namanga | 25 April 2026</title>
                   <meta
                     name="description"
                     content="Join God's Plan Charity outreach event at Rejoice Homekids Center, Namanga on 25 April 2026 supporting orphaned children with education, food, and computer skills."
                 />    
+                <p>
+                    God's Plan Charity will visit Rejoice Homekids Center on 25 April 2026
+                    to support children with education, food, and computer skills.
+                </p>
                 </Helmet> 
                 <AlertBar />
                 <Navbar />
